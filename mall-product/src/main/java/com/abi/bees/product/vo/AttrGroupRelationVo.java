ec69066d6e4df517df5f7ac2b81eeceaf4937df4
shilinfeng <linfeng516666@163.com>
2022-12-25 23:11:52 +0800

    2022/12/25提交

---
+++ mall-product/src/main/java/com/abi/bees/product/vo/AttrGroupRelationVo.java
@@ -0,0 +1,21 @@
+package com.abi.bees.product.vo;
+
+import lombok.Data;
+
+/**
+ * @author SLF
+ * @date 2022/12/25
+ */
+@Data
+public class AttrGroupRelationVo {
+
+    /**
+     * 属性id
+     */
+    private Long attrId;
+
+    /**
+     * 属性分组id
+     */
+    private Long attrGroupId;
+}
