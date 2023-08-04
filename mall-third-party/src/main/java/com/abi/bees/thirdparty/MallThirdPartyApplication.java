d96304c189a9226aa365a300b662fbd42d790c33
v_SLF <linfeng516666@163.com>
2023-08-04 18:27:36 +0800

    OSS获取服务端签名以及在网关配置路由

--- mall-third-party/src/main/java/com/abi/bees/thirdparty/MallThirdPartyApplication.java
+++ mall-third-party/src/main/java/com/abi/bees/thirdparty/MallThirdPartyApplication.java
@@ -2,10 +2,11 @@ package com.abi.bees.thirdparty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MallThirdPartyApplication {
 
 	public static void main(String[] args) {
